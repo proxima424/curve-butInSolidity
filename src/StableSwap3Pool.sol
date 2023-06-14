277504b070bd20556ee346befa628b16f563db0a
Darpit Rangari <95137001+proxima424@users.noreply.github.com>
2023-06-14 05:45:20 +0530

    wowwwwwwwwwwwwwww

--- src/StableSwap3Pool.sol
+++ src/StableSwap3Pool.sol
@@ -23,6 +23,10 @@ contract StableSwap3Pool {
 
     event AddLiquidity(address indexed provider,uint256[3] token_amounts, uint256[3] fees, uint256 invariant, uint256 token_supply);
 
+    event RemoveLiquidityImbalance(address indexed provider, uint256[3] token_amounts, uint256[3] fees, uint256 invariant, uint256 token_supply);
+
+    event RemoveLiquidityOne(address indexed provider, uint256 token_amount, uint256 coin_amount);
+
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                         ERRORS                             */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
@@ -464,8 +468,374 @@ contract StableSwap3Pool {
         emit TokenExchange(msg.sender,i,dx,j,dy);
     }
 
+    function remove_liquidity(uint256 _amount, uint256[3] min_amounts) external nonReentrant{
+        uint256 total_supply = token.totalSupply();
+        uint256[3] amounts;
+        // Fees are unused but we've got them historically in event
+        uint256[3] fees;
+
+        for(uint256 i;i<3;){
+            uint256 value =  balances[i] * _amount / total_supply;
+            require(value >= min_amounts[i],"Withdrawal resulted in fewer coins than expected");
+            balances[i] -= value;
+            amounts[i] = value;
+
+            bool success = IERC20(coins[i]).transfer(msg.sender,value);
+            require(success);
+            unchecked{
+                ++i;
+            }
+        }
+
+        token.burnFrom(msg.sender,_amount);
+
+        emit RemoveLiquidity(msg.sender,amounts,fees,total_supply - _amount);
+    }
+
+    function remove_liquidity_imbalance(uint256[3] amounts, uint256 max_burn_amount) external nonReentrant{
+        require(!is_killed);
+        uint256 token_supply  = token.totalSupply();
+        require(token_supply!=0);
+
+        uint256 _fee = fee * 3 / (4 * (2));
+        uint256 _admin_fee = admin_fee;
+        uint256 amp = _A();
+
+        uint256[3] old_balances = balances;
+        uint256[3] new_balances = old_balances;
+
+        uint256 D0 = get_D_mem(old_balances, amp);
+
+        for(uint256 i;i<3;){
+            new_balances[i] -= amounts[i];
+            unchecked {
+                ++i;
+            }
+        }
+
+        uint256 D1 = get_D_mem(new_balances, amp);
+
+        uint256[3] fees;
+
+        for(uint256 i;i<3;){
+            uint256 ideal_balances = D1 * old_balances[i] / D0;
+            uint256 difference;
+
+            if (ideal_balance > new_balances[i]){
+                difference = ideal_balance - new_balances[i];
+            }
+            else{
+                difference = new_balances[i] - ideal_balance;
+            }
+
+            fees[i] = _fee * difference / FEE_DENOMINATOR;
+            balances[i] = new_balances[i] - (fees[i] * _admin_fee / FEE_DENOMINATOR);
+
+            new_balances[i] -= fees[i];
 
 
+            unchecked {
+                ++i;
+            }
+        }
 
+        uint256 D2 = get_D_mem(new_balances, amp);
+
+        uint256 token_amount =  (D0 - D2) * token_supply / D0;
+        require(token_amount!=0);
+
+        token_amount++; //  In case of rounding errors - make it unfavorable for the "attacker"
+
+        require(token_amount <= max_burn_amount, "Slippage screwed you");
+
+        token.burnFrom(msg.sender, token_amount);
+
+        for ( uint256 i; i<3;){
+            if(amounts[i] !=0 ){
+
+                bool success = IERC20(coins[i]).transfer(msg.sender,amounts[i]);
+                require(success);
+
+            }
+            unchecked {
+                ++i;
+            }
+        }
+
+        emit RemoveLiquidityImbalance(msg.sender, amounts, fees, D1, token_supply - token_amount);
+    }
+
+    function get_y(uint256 A_, int128 i, uint256[3] xp, uint256 D) internal view returns(uint256){
+
+        //  Calculate x[i] if one reduces D from being calculated for xp to D
+        //  Done by solving quadratic equation iteratively.
+        //  x_1**2 + x1 * (sum' - (A*n**n - 1) * D / (A * n**n)) = D ** (n + 1) / (n ** (2 * n) * prod' * A)
+        //  x_1**2 + b*x_1 = c
+        //  x_1 = (x_1**2 + c) / (2*x_1 + b)
+
+        // x in the input is converted to the same price/precision
+
+        require(i>=0);
+        require(i<3);
+
+        uint256 c = D;
+        uint256 S_ = 0;
+        uint256 Ann = A_*3;
+        uint256 _x = 0 ;
+
+        for(uint256 j;j<3;){
+            if(_i != i){
+                _x = xp[_i];
+            }
+
+            S_ += _x;
+            c = c * D / (_x * 3);
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        c = c * D / (Ann * 3);
+
+        uint256 b = S_ + D / Ann;
+        uint256 y_prev;
+        uint256 y = D;
+
+        for(uint256 j;j<255;){
+            y_prev = y;
+            y = (y*y + c) / (2 * y + b - D);
+
+            // Equality with the precision of 1
+
+            if( y> y_prev){
+                if( y- y_prev ){
+                    break;
+                }
+            }
+            else{
+                if ( y_prev - y){
+                    break;
+                }
+            }
+            unchecked {
+                ++i;
+            }
+        }
+
+        return y;
+    }
+
+    function _calc_withdraw_one_coin(uint256 _token_amount, int128 i) internal view returns(uint256,uint256){
+
+        // First we need to calculate
+        // * Get current D
+        // Solve Eqn against y_i for D - _token_amount
+
+        uint256 amp = _A();
+        uint256 _fee = (fee*3) / 8;
+        uint256[3] precisions = PRECISION_MUL;
+
+        uint256 total_supply = token.totalSupply();
+
+        uint256[3] xp = _xp();
+
+        uint256 D0 = get_D(xp, amp);
+        uint256 D1 = D0 - _token_amount * D0 / total_supply;
+
+        uint256[3] xp_reduced = xp;
+
+        uint256 new_y = get_y_D(amp, i, xp, D1);
+        uint256 dy_0 = (xp[i] - new_y) / precisions[i]  // w/o fees
+
+        for(uint256 j;j<3;){
+            uint256 dx_expected;
+            if(j==i){
+                dx_expected = xp[j] * D1 / D0 - new_y;
+            }
+            else{
+                dx_expected = xp[j] - xp[j] * D1 / D0;
+            }
+            xp_reduced[j] -= _fee * dx_expected / FEE_DENOMINATOR;
+            unchecked {
+                ++i;
+            }
+        }
+
+        uint256 dy = xp_reduced[i] - get_y_D(amp, i, xp_reduced, D1);
+        dy = (dy - 1) / precisions[i]  // Withdraw less to account for rounding errors
+
+        return dy, dy_0 - dy
+    }
+
+    function calc_withdraw_one_coin ( uint256 _token_amount, int128 i) returns(uint256){
+        return _calc_withdraw_one_coin(_token_amount, i)[0];
+    }
+
+    function remove_liquidity_one_coin(uint256 _token_amount, int128 i, uint256 min_amount){
+        // Remove _amount of liquidity all in a form of coin i
+
+        require(!is_killed);
+
+        (uint256 dy, uint256 dy_fee) = _calc_withdraw_one_coin(_token_amount, i);
+
+        require(dy>=min_amount,"Not enough coins removed");
+
+        balances[i] -= (dy + dy_fee * admin_fee / FEE_DENOMINATOR);
+        token.burnFrom(msg.sender, _token_amount);
+
+        bool success = IERC20(coins[i]).transfer(msg.sender,dy);
+
+        require(success);
+
+        emit RemoveLiquidityOne(msg.sender, _token_amount, dy);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         ADMIN FUNCTIONS                    */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function ramp_A(uint256 _future_A, uint256 _future_time) external {
+
+        require(msg.sender == owner);
+        require(block.timestamp >= initial_A_time + MIN_RAMP_TIME);
+        require(_future_time >= block.timestamp + MIN_RAMP_TIME, "insufficient time");
+
+        uint256 _initial_A = _A();
+
+        require( (_future_A > 0) && (_future_A < MAX_A) );
+
+        require(((_future_A >= _initial_A) && (_future_A <= _initial_A * MAX_A_CHANGE))  ||   ( (_future_A < _initial_A) && (_future_A * MAX_A_CHANGE >= _initial_A) ));
+
+        initial_A = _initial_A;
+        future_A = _future_A;
+        initial_A_time = block.timestamp;
+        future_A_time = _future_time;
+
+        emit RampA(_initial_A, _future_A, block.timestamp, _future_time);
+    }
+
+    function stop_ramp_A() external {
+        require(msg.sender == owner);
+
+        uint256 current_A = _A();
+        initial_A = current_A;
+        future_A = current_A;
+        initial_A_time = block.timestamp;
+        future_A_time = block.timestamp;
+
+        // now (block.timestamp < t1) is always False, so we return saved A
+
+        emit StopRampA(current_A, block.timestamp);
+    }
+
+    function commit_new_fee(uint256 new_fee, uint256 new_admin_fee ) external{
+
+        require(msg.sender == owner);
+        require(admin_actions_deadline == 0);
+        require(new_fee <= MAX_FEE);
+        require(new_admin_fee <= MAX_ADMIN_FEE);
+
+        uint256 _deadline = block.timestamp + ADMIN_ACTIONS_DELAY;
+        admin_actions_deadline = _deadline;
+        future_fee = new_fee;
+        future_admin_fee = new_admin_fee;
+
+        emit CommitNewFee(_deadline, new_fee, new_admin_fee);
+    }
+
+    function apply_new_fee() external {
+        require(msg.sender == owner);
+        require(block.timestamp >= admin_actions_deadline);
+        require(admin_actions_deadline != 0);
+
+        admin_actions_deadline = 0;
+        uint256 _fee = future_fee;
+        uint256 _admin_fee = future_admin_fee;
+        fee = _fee;
+        admin_fee = _admin_fee;
+
+        emit NewFee(_fee, _admin_fee);
+
+    }
+
+    function revert_new_parameters() external {
+        require(msg.sneder == owner);
+        admin_actions_deadline = 0;
+    }
+
+    function commit_transfer_ownership(address _owner) external {
+        require(msg.sender == owner);
+        require(transfer_ownership_deadline == 0, "active transfer");
+
+        uint256 _deadline = block.timestamp + ADMIN_ACTIONS_DELAY;
+        transfer_ownership_deadline = _deadline;
+        future_owner = _owner;
+
+        emit CommitNewAdmin(_deadline, _owner);
+    }
+
+    function apply_transfer_ownership() external {
+        require(msg.sender == owner);
+        require(block.timestamp >= transfer_ownership_deadline, "insufficient time" );
+        require(transfer_ownership_deadline != 0 , "no active transfer");
+
+        transfer_ownership_deadline = 0;
+        address _owner = future_owner;
+        owner = _owner;
+
+        emit NewAdmin(_owner);
+    }
+
+    function revert_transfer_ownership() external {
+        require(msg.sender == owner);
+        transfer_ownership_deadline = 0;
+    }
+
+    function admin_balances(uint256 i) external view returns(uint256){
+        return IERC20(coins[i]).balanceOf(address(this)) - balances[i];
+    }
+
+    function withdraw_admin_fees() external {
+        require(msg.sender == owner);
+
+        for(uint256 i;i<3;){
+
+            address c = coins[i];
+            uint256 value = IERC20(coins[i]).balanceOf(address(this)) - balances[i];
+
+            if(value>0){
+                bool success = IERC20(coins[i]).transfer(msg.sender,value);
+                require(success);
+            }
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function donate_admin_fees() external {
+        require(msg.sender == owner);
+
+        for( uint256 i;i<3;){
+            balances[i] = IERC20(coins[i]).balanceOf(address(this));
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function kill_me() external {
+        require(msg.sender == owner);
+        require(kill_deadline > block.timestamp);
+
+        is_killed = true;
+    }
+
+    function unkill_me() external {
+        require(msg.sender == owner);
+        is_killed = false;
+    }
 
 }
